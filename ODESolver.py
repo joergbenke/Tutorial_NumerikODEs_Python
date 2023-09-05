d934e8ca9b64aa602656333004a9c832f22ff49e
Joerg Benke <j.benke@fz-juelich.de>
2023-09-05 20:39:45 +0200

    New ODE addad (logarithmic growth); PEP8 with flake8

--- ODESolver.py
+++ ODESolver.py
@@ -1,89 +1,81 @@
-import numpy as np
-
 class ODESolver:
     def euler_explicit(self, t0, y0, h, n, f):
         """
         Implementation of the explicit euler algorithm
         """
-        
-        t = t0
-        y = y0
-        result = [(t,y)]
-    	
+
+        t, y = t0, y0
+        result = [(t, y)]
+
         for i in range(n):
-            t += h		
-            y += h * f(t,y)
-            result.append((t, y))	
-    	
-        return result    
-        
-    def midpointrule( self, t0, y0, h, n, f):
+            t += h
+            y += h * f(t, y)
+            result.append((t, y))
+
+        return result
+
+    def midpointrule(self, t0, y0, h, n, f):
         """
-        Implementation of the midpoint rule algorithm (2nd order Runge-Kutta agorithm)
+        Implementation of the midpoint rule algorithm (2nd order
+        Runge-Kutta agorithm)
         """
-        
-        t = t0
-        y = y0
-        result = [(t,y)]
-        
+
+        t, y = t0, y0
+        result = [(t, y)]
+
         for i in range(n):
             t += h
             y_tmp = y + 0.5 * h * f(t, y)
-            y += h * f(t + 0.5 * h, y_tmp )
+            y += h * f(t + 0.5 * h, y_tmp)
             result.append((t, y))
-            
-        return result        
-        
-        
+
+        return result
+
     def heun(self, t0, y0, h, n, f):
         """
         Implementation of the heun algorithm (2nd order Runge-Kutta agorithm)
         """
-        
-        t = t0
-        y = y0
-        k1 = 0.0
-        k2 = 0.0
-        result = [(t,y)]
-        
+
+        t, y = t0, y0
+        k1 = k2 = 0.0
+        result = [(t, y)]
+
         for i in range(n):
             t += h
             k1 = f(t, y)
-            
+
             y_tmp = y + h * f(t, y)
             k2 = f(t + h, y_tmp)
-            
+
             y += 0.5 * h * (k1 + k2)
             result.append((t, y))
-            
-        return result     
-        
+
+        return result
+
     def ClassicalRK4(self, t0, y0, h, n, f):
         """
         Implementation of the heun algorithm (2nd order Runge-Kutta agorithm)
         """
-        
-        t = t0
-        y = y0
+
+        t, y = t0, y0
         k1 = k2 = k3 = k4 = 0.0
-        
-        result = [(t,y)]
-        
+
+        result = [(t, y)]
+
         for i in range(n):
             t += h
             k1 = f(t, y)
-            
+
             y_tmp = y + 0.5 * h * k1
             k2 = f(t + 0.5 * h, y_tmp)
-            
+
             y_tmp = y + 0.5 * h * k2
             k3 = f(t + 0.5 * h, y_tmp)
-            
+
             y_tmp = y + 0.5 * h * k3
             k4 = f(t + h, y_tmp)
-            
+
             y += h * 1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
             result.append((t, y))
-            
-        return result                    	
-    	 
+
+        return result
--- main.py
+++ main.py
@@ -1,49 +1,52 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import ODESolver as odes
 import math
-	
+
 if __name__ == "__main__":
-    t0 = 0
-    y0 = 1
+    t0, y0 = 0, 1
     h = 0.1
     n = 40
-    
+
     obj = odes.ODESolver()
-    #result_euler_explicit = obj.euler_explicit(t0, y0, h, n, lambda t, y:-math.sin(t))
-    #result_midpointrule = obj.midpointrule(t0, y0, h, n, lambda t, y:-math.sin(t))
-    #result_heun = obj.heun(t0, y0, h, n, lambda t, y:-math.sin(t))
-    
-    result_euler_explicit = obj.euler_explicit(t0, y0, h, n, lambda t, y:-y)
-    result_midpointrule = obj.midpointrule(t0, y0, h, n, lambda t, y:-y)
-    result_heun = obj.heun(t0, y0, h, n, lambda t, y:-y)
-    result_rk4 = obj.ClassicalRK4(t0, y0, h, n, lambda t, y:-y)
-    
-    # print the graphic
+
+    # Defining the ODE which should be integrated
+    # fct = lambda t, y:-math.sin(t)  #constant ODE
+
+    k = -1
+    def fct(t, y): return k * y  # exponential growth
+
+    # k, G = 1, 2
+    # def fct(t, y): return k * y * (G - y)
+
+    result_euler_explicit = obj.euler_explicit(t0, y0, h, n, fct)
+    result_midpointrule = obj.midpointrule(t0, y0, h, n, fct)
+    result_heun = obj.heun(t0, y0, h, n, fct)
+    result_rk4 = obj.ClassicalRK4(t0, y0, h, n, fct)
+
+    # print the figure
     t = []
     y_heun = []
     y_euler_explicit = []
     y_midpointrule = []
     y_rk4 = []
     y_exact = []
-   
+
     for i in range(n):
         t.append(result_heun[i][0])
+
         y_euler_explicit.append(result_euler_explicit[i][1])
         y_midpointrule.append(result_midpointrule[i][1])
         y_heun.append(result_heun[i][1])
         y_rk4.append(result_rk4[i][1])
-        y_exact.append(math.exp(-i * h))
-        	    
-#    plt.plot(t, y_heun, label="Heun method")
-    plt.plot(t, y_euler_explicit, label="Euler explicit method")    
-#    plt.plot(t, y_midpointrule, label="Midpoint rule") 
-    plt.plot(t, y_rk4, label="Classic RK4")       
-    #plt.plot(t, y_exact, label="Exact cos")
-    plt.plot(t, y_exact, label="Exact solution y'=-y")
-    plt.legend()
+        y_exact.append(math.exp(-i * h))  #exact solution for y'=-y
+        # y_exact.append(G * (1 / (1 + math.exp(-k * G * i * h) * (G / y0 - 1))))  # exact solution for y'=k*y*)G-y))
 
+    # plt.plot(t, y_heun, label="Heun method")
+    plt.plot(t, y_euler_explicit, label="Euler explicit method")
+    # plt.plot(t, y_midpointrule, label="Midpoint rule")
+    plt.plot(t, y_rk4, label="Classic RK4")
+    # plt.plot(t, y_exact, label="Exact cos")
+
+    # plt.plot(t, y_exact, label="Exact solution y'=-y")
+    plt.legend()
     plt.show()
-        
-         
-         
-    
