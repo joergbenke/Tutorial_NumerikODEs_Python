e8fc64227e6df6dbe41593a3d0ce877ac8e891b6
Joerg Benke <j.benke@fz-juelich.de>
2023-09-05 19:27:58 +0200

    RK4 added, .gitignore added, tested with another ODE (y'=-y)

--- ODESolver.py
+++ ODESolver.py
@@ -56,5 +56,34 @@ class ODESolver:
             y += 0.5 * h * (k1 + k2)
             result.append((t, y))
             
-        return result            	
+        return result     
+        
+    def ClassicalRK4(self, t0, y0, h, n, f):
+        """
+        Implementation of the heun algorithm (2nd order Runge-Kutta agorithm)
+        """
+        
+        t = t0
+        y = y0
+        k1 = k2 = k3 = k4 = 0.0
+        
+        result = [(t,y)]
+        
+        for i in range(n):
+            t += h
+            k1 = f(t, y)
+            
+            y_tmp = y + 0.5 * h * k1
+            k2 = f(t + 0.5 * h, y_tmp)
+            
+            y_tmp = y + 0.5 * h * k2
+            k3 = f(t + 0.5 * h, y_tmp)
+            
+            y_tmp = y + 0.5 * h * k3
+            k4 = f(t + h, y_tmp)
+            
+            y += h * 1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+            result.append((t, y))
+            
+        return result                    	
     	 
--- main.py
+++ main.py
@@ -9,16 +9,21 @@ if __name__ == "__main__":
     n = 40
     
     obj = odes.ODESolver()
-    result_euler_explicit = obj.euler_explicit(t0, y0, h, n, lambda t, y:-math.sin(t))
-    result_midpointrule = obj.midpointrule(t0, y0, h, n, lambda t, y:-math.sin(t))
-    result_heun = obj.heun(t0, y0, h, n, lambda t, y:-math.sin(t))
+    #result_euler_explicit = obj.euler_explicit(t0, y0, h, n, lambda t, y:-math.sin(t))
+    #result_midpointrule = obj.midpointrule(t0, y0, h, n, lambda t, y:-math.sin(t))
+    #result_heun = obj.heun(t0, y0, h, n, lambda t, y:-math.sin(t))
     
+    result_euler_explicit = obj.euler_explicit(t0, y0, h, n, lambda t, y:-y)
+    result_midpointrule = obj.midpointrule(t0, y0, h, n, lambda t, y:-y)
+    result_heun = obj.heun(t0, y0, h, n, lambda t, y:-y)
+    result_rk4 = obj.ClassicalRK4(t0, y0, h, n, lambda t, y:-y)
     
     # print the graphic
     t = []
     y_heun = []
     y_euler_explicit = []
     y_midpointrule = []
+    y_rk4 = []
     y_exact = []
    
     for i in range(n):
@@ -26,12 +31,15 @@ if __name__ == "__main__":
         y_euler_explicit.append(result_euler_explicit[i][1])
         y_midpointrule.append(result_midpointrule[i][1])
         y_heun.append(result_heun[i][1])
-        y_exact.append(math.cos(i * h))
+        y_rk4.append(result_rk4[i][1])
+        y_exact.append(math.exp(-i * h))
         	    
-    plt.plot(t, y_heun, label="Heun method")
+#    plt.plot(t, y_heun, label="Heun method")
     plt.plot(t, y_euler_explicit, label="Euler explicit method")    
-    plt.plot(t, y_midpointrule, label="Midpoint rule")    
-    plt.plot(t, y_exact, label="Exact cos")
+#    plt.plot(t, y_midpointrule, label="Midpoint rule") 
+    plt.plot(t, y_rk4, label="Classic RK4")       
+    #plt.plot(t, y_exact, label="Exact cos")
+    plt.plot(t, y_exact, label="Exact solution y'=-y")
     plt.legend()
 
     plt.show()
