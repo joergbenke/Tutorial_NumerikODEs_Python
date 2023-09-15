f5a9bb19f74624cb72013688d0f72dc550507012
Joerg Benke <j.benke@fz-juelich.de>
2023-09-15 20:19:28 +0200

    Works now for dim > 1, but not for dim ==1

--- ODESolver.py
+++ ODESolver.py
@@ -6,6 +6,29 @@ class ODESolver:
         Implementation of the explicit euler algorithm
         """
         t = np.zeros(n + 1, dtype='f')
+        neq = 1
+               
+        if isinstance(y0, (int, float)):
+            y = np.zeros(n + 1, dtype='f')
+            y[0] = y0
+        else:
+            neq = len(y0)
+            y = np.zeros((n + 1, neq), dtype='f')
+            y[0,:] = y0
+                
+        t[0] = t0
+        for k in range(n):
+            t[k + 1] = t[k] + h
+            y[k + 1, :] = y[k, :] + h * f(t[k], y[k, :])
+            print(t[k+1], y[k + 1, :])
+        return t, y
+
+    def midpointrule(self, t0, y0, h, n, f):
+        """
+        Implementation of the midpoint rule algorithm (2nd order
+        Runge-Kutta agorithm)
+        """
+        t = np.zeros(n + 1, dtype='f')
         
         if isinstance(y0, (int, float)):
             y = np.zeros(n + 1, dtype='f')
@@ -18,47 +41,37 @@ class ODESolver:
         
         for k in range(n):
             t[k + 1] = t[k] + h
-            y[k + 1, :] = y[k, :] + h * f(t[k], y[k, :])
+            y[k + 1, :] = y[k, :] + h * f(t[k] + 0.5 * h, y[k, :] + 0.5 * h * f(t[k], y[k]))
         print(y)
         return t, y
-
-    def midpointrule(self, t0, y0, h, n, f):
-        """
-        Implementation of the midpoint rule algorithm (2nd order
-        Runge-Kutta agorithm)
-        """
-
-        t, y = t0, y0
-        result = [(t, y)]
-
-        for i in range(n):
-            t += h
-            y_tmp = y + 0.5 * h * f(t, y)
-            y += h * f(t + 0.5 * h, y_tmp)
-            result.append((t, y))
-
-        return result
+            
 
     def heun(self, t0, y0, h, n, f):
         """
         Implementation of the heun algorithm (2nd order Runge-Kutta agorithm)
         """
-
-        t, y = t0, y0
-        k1 = k2 = 0.0
-        result = [(t, y)]
-
-        for i in range(n):
-            t += h
-            k1 = f(t, y)
-
-            y_tmp = y + h * f(t, y)
-            k2 = f(t + h, y_tmp)
-
-            y += 0.5 * h * (k1 + k2)
-            result.append((t, y))
-
-        return result
+        t = np.zeros(n + 1, dtype='f')
+               
+        if isinstance(y0, (int, float)):
+            y = np.zeros(n + 1, dtype='f')
+        else:
+            neq = len(y0)
+            y = np.zeros((n + 1, neq), dtype='f')
+                
+        t[0] = t0
+        y[0,:] = y0
+        
+        for k in range(n):
+            k1 = f(t[k], y[k, :])
+            
+            y_tmp = y[k, :] + h * f(t[k + 1], y[k + 1, :])
+            k2 = f(t[k + 1], y_tmp)
+             
+            t[k + 1] = t[k] + h 
+            y[k + 1, :] = y[k, :] + h * 0.5 * (k1 + k2)
+        print(y)
+        return t, y
+           
 
     def ClassicalRK4(self, t0, y0, h, n, f):
         """
--- main.py
+++ main.py
@@ -4,47 +4,54 @@ import ODESolver as odes
 import math
 
 if __name__ == "__main__":
+
+    # Set parameters and initial conditions
     t0 = 0.0
-    y0 = 1
+    tf = 4
     h = 0.1
-    n = 400
-    dim = 2
+    n = int(((tf - t0) / h ))
+    # y0 = np.array([1,1])
+    y0 = 1
 
     obj = odes.ODESolver()
 
-    # Defining the ODE which should be integrated
+    #
+    # Defining the ODE/system of ODEs which should be integrated
+    
     # def fct(t, y): -math.sin(t)  #constant ODE
 
-    # k = -1
-    # def fct(t, y): 
-    #    return k * y  # exponential growth
+    k = -1
+    def fct(t, y): 
+        return k * y  # exponential growth
 
     # k, G = 1, 2
     # def fct(t, y): return k * y * (G - y)
 
-    # g = -9.8
-    # v = 0.0
-    # def fct(t, y): return np.array([y[1], -g])  
+    #g = -9.8
+    #def fct(t, y): 
+    #    return np.array([y[1], g])  
     
-    def fct(t, y):
-        return np.array([y[1], -y[0]])
+    # ODE y'' + y = 0
+    # def fct(t, y):
+    #    return np.array([y[1], -y[0]])
 
-    y0 = np.array([0,1])
+    #
+    # Integrating the ODEs 
     time_array, result_euler_explicit = obj.euler_explicit(t0, y0, h, n, fct)
-    #result_midpointrule = obj.midpointrule(t0, y0, h, n, fct)
-    #result_heun = obj.heun(t0, y0, h, n, fct)
+    time_array, result_midpointrule = obj.midpointrule(t0, y0, h, n, fct)
+    time_array, result_heun = obj.heun(t0, y0, h, n, fct)
     #result_rk4 = obj.ClassicalRK4(t0, y0, h, n, fct)
 
     # print the figure
-    y_exact = np.zeros(n + 1, dtype = 'f')
-    for i in range(n + 1):
-        y_exact[i] = math.exp(-i * h)  #exact solution for y'=-y
+    # y_exact = np.zeros(n + 1, dtype = 'f')
+    # for i in range(n + 1):
+        # y_exact[i] = math.exp(-i * h)  #exact solution for y'=-y
         # y_exact.append(G * (1 / (1 + math.exp(-k * G * i * h) * (G / y0 - 1))))  # exact solution for y'=k*y*)G-y))
 
     # plt.plot(t, y_heun, label="Heun method")
-    plt.plot(time_array, result_euler_explicit, label="Euler explicit method")
-    # plt.plot(t, y_midpointrule, label="Midpoint rule")
-    # plt.plot(t, y_rk4, label="Classic RK4")
+    plt.plot(time_array, result_euler_explicit, label=["height (expl Euler)", "velocity (expl Euler"])
+    plt.plot(time_array, result_midpointrule, label=["height (Midpointrule)", "velocity (Midpointrule)"])
+    plt.plot(time_array, result_heun, label=["height (Heun)", "velocity (Heun)"])
     # plt.plot(time_array, y_exact, label="Exact solution (exp)")
 
     # plt.plot(t, y_exact, label="Exact solution y'=-y")
