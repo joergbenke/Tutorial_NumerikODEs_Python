e043623316b96ba35f34c985f966b1d49f0bb78e
Joerg Benke <j.benke@fz-juelich.de>
2023-09-23 18:34:21 +0200

    Adaption to scalar and array version

--- ODESolver.py
+++ ODESolver.py
@@ -17,10 +17,16 @@ class ODESolver:
             y[0,:] = y0
                 
         t[0] = t0
-        for k in range(n):
-            t[k + 1] = t[k] + h
-            y[k + 1, :] = y[k, :] + h * f(t[k], y[k, :])
-            print(t[k+1], y[k + 1, :])
+        if neq > 1:
+            for k in range(n):
+                t[k + 1] = t[k] + h
+                y[k + 1, :] = y[k, :] + h * f(t[k], y[k, :])
+                print(t[k+1], y[k + 1, :])
+        else:
+            for k in range(n):
+                t[k + 1] = t[k] + h
+                y[k + 1] = y[k] + h * f(t[k], y[k])
+                print(t[k+1], y[k + 1])      
         return t, y
 
     def midpointrule(self, t0, y0, h, n, f):
@@ -29,19 +35,25 @@ class ODESolver:
         Runge-Kutta agorithm)
         """
         t = np.zeros(n + 1, dtype='f')
+        neq = 1
         
         if isinstance(y0, (int, float)):
             y = np.zeros(n + 1, dtype='f')
+            y[0] = y0
         else:
             neq = len(y0)
             y = np.zeros((n + 1, neq), dtype='f')
+            y[0,:] = y0            
                 
         t[0] = t0
-        y[0,:] = y0
-        
-        for k in range(n):
-            t[k + 1] = t[k] + h
-            y[k + 1, :] = y[k, :] + h * f(t[k] + 0.5 * h, y[k, :] + 0.5 * h * f(t[k], y[k]))
+        if neq > 1:
+            for k in range(n):
+                t[k + 1] = t[k] + h
+                y[k + 1, :] = y[k, :] + h * f(t[k] + 0.5 * h, y[k, :] + 0.5 * h * f(t[k], y[k, :]))
+        else:
+            for k in range(n):
+                t[k + 1] = t[k] + h
+                y[k + 1] = y[k] + h * f(t[k] + 0.5 * h, y[k] + 0.5 * h * f(t[k], y[k]))            
         print(y)
         return t, y
             
@@ -51,24 +63,36 @@ class ODESolver:
         Implementation of the heun algorithm (2nd order Runge-Kutta agorithm)
         """
         t = np.zeros(n + 1, dtype='f')
+        neq = 1
                
         if isinstance(y0, (int, float)):
             y = np.zeros(n + 1, dtype='f')
+            y[0] = y0
         else:
             neq = len(y0)
             y = np.zeros((n + 1, neq), dtype='f')
+            y[0,:] = y0            
                 
         t[0] = t0
-        y[0,:] = y0
-        
-        for k in range(n):
-            k1 = f(t[k], y[k, :])
+        if neq > 1:
+            for k in range(n):
+                t[k + 1] = t[k] + h 
+                k1 = f(t[k], y[k, :])
             
-            y_tmp = y[k, :] + h * f(t[k + 1], y[k + 1, :])
-            k2 = f(t[k + 1], y_tmp)
+                y_tmp = y[k, :] + h * f(t[k + 1], y[k + 1, :])
+                k2 = f(t[k + 1], y_tmp)
              
-            t[k + 1] = t[k] + h 
-            y[k + 1, :] = y[k, :] + h * 0.5 * (k1 + k2)
+                y[k + 1, :] = y[k, :] + h * 0.5 * (k1 + k2)
+        else:
+            for k in range(n):
+                t[k + 1] = t[k] + h 
+                k1 = f(t[k], y[k])
+            
+                y_tmp = y[k] + h * f(t[k + 1], y[k + 1])
+                k2 = f(t[k + 1], y_tmp)
+             
+                y[k + 1] = y[k] + h * 0.5 * (k1 + k2)
+                
         print(y)
         return t, y
            
@@ -77,26 +101,48 @@ class ODESolver:
         """
         Implementation of the heun algorithm (2nd order Runge-Kutta agorithm)
         """
+        t = np.zeros(n + 1, dtype='f')
+        neq = 1
+               
+        if isinstance(y0, (int, float)):
+            y = np.zeros(n + 1, dtype='f')
+            y[0] = y0
+        else:
+            neq = len(y0)
+            y = np.zeros((n + 1, neq), dtype='f')
+            y[0,:] = y0     
 
-        t, y = t0, y0
-        k1 = k2 = k3 = k4 = 0.0
+        t[0] = t0
+        if neq > 1:
+            for k in range(n):
+                t[k + 1] = t[k] + h
+                k1 = f(t[k], y[k, :])
 
-        result = [(t, y)]
+                y_tmp = y[k, :] + 0.5 * h * k1
+                k2 = f(t[k] + 0.5 * h, y_tmp)
 
-        for i in range(n):
-            t += h
-            k1 = f(t, y)
+                y_tmp = y[k, :] + 0.5 * h * k2
+                k3 = f(t[k] + 0.5 * h, y_tmp)
 
-            y_tmp = y + 0.5 * h * k1
-            k2 = f(t + 0.5 * h, y_tmp)
+                y_tmp = y[k, :] + 0.5 * h * k3
+                k4 = f(t[k + 1], y_tmp)
 
-            y_tmp = y + 0.5 * h * k2
-            k3 = f(t + 0.5 * h, y_tmp)
+                y[k + 1, :] = y[k, :] + h * 1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        else:
+            for k in range(n):
+                t[k + 1] = t[k] + h
+                k1 = f(t[k], y[k])
 
-            y_tmp = y + 0.5 * h * k3
-            k4 = f(t + h, y_tmp)
+                y_tmp = y[k] + 0.5 * h * k1
+                k2 = f(t[k] + 0.5 * h, y_tmp)
 
-            y += h * 1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
-            result.append((t, y))
+                y_tmp = y[k] + 0.5 * h * k2
+                k3 = f(t[k] + 0.5 * h, y_tmp)
 
-        return result
+                y_tmp = y[k] + 0.5 * h * k3
+                k4 = f(t[k + 1], y_tmp)
+
+                y[k + 1] = y[k] + h * 1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+
+        print(y)
+        return t, y
--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     tf = 4
     h = 0.1
     n = int(((tf - t0) / h ))
-    # y0 = np.array([1,1])
-    y0 = 1
+    y0 = np.array([1,1])
+    # y0 = 1 # scalar case 
 
     obj = odes.ODESolver()
 
@@ -20,16 +20,19 @@ if __name__ == "__main__":
     
     # def fct(t, y): -math.sin(t)  #constant ODE
 
-    k = -1
-    def fct(t, y): 
-        return k * y  # exponential growth
+    # k = -1
+    # label = "exponential growth"
+    # def fct(t, y): 
+    #    return k * y  # exponential growth
 
     # k, G = 1, 2
-    # def fct(t, y): return k * y * (G - y)
+    # def fct(t, y): 
+    #    return k * y * (G - y)
 
-    #g = -9.8
-    #def fct(t, y): 
-    #    return np.array([y[1], g])  
+    g = -9.8
+    label = "falling body"
+    def fct(t, y): 
+        return np.array([y[1], g])  
     
     # ODE y'' + y = 0
     # def fct(t, y):
@@ -40,7 +43,24 @@ if __name__ == "__main__":
     time_array, result_euler_explicit = obj.euler_explicit(t0, y0, h, n, fct)
     time_array, result_midpointrule = obj.midpointrule(t0, y0, h, n, fct)
     time_array, result_heun = obj.heun(t0, y0, h, n, fct)
-    #result_rk4 = obj.ClassicalRK4(t0, y0, h, n, fct)
+    time_array, result_rk4 = obj.ClassicalRK4(t0, y0, h, n, fct)
+
+    #
+    # Printing the solutions
+    if label == "exponential growth":
+        plt.plot(time_array, result_euler_explicit, label="expl Euler")
+        plt.plot(time_array, result_midpointrule, label="Midpointrule")
+        plt.plot(time_array, result_heun, label="Heun")
+        plt.plot(time_array, result_rk4, label="RK4")
+        # plt.plot(time_array, y_exact, label="Exact solution (exp)")
+        
+    if label == "falling body":
+        plt.plot(time_array, result_euler_explicit, label=["height (expl Euler)", "velocity (expl Euler"])
+        plt.plot(time_array, result_midpointrule, label=["height (Midpointrule)", "velocity (Midpointrule)"])
+        plt.plot(time_array, result_heun, label=["height (Heun)", "velocity (Heun)"])
+        plt.plot(time_array, result_rk4, label=["height (RK4)", "velocity (RK4)"])
+        # plt.plot(time_array, y_exact, label="Exact solution (exp)")         
+        # plt.plot(t, y_exact, label="Exact solution y'=-y")
 
     # print the figure
     # y_exact = np.zeros(n + 1, dtype = 'f')
@@ -48,12 +68,5 @@ if __name__ == "__main__":
         # y_exact[i] = math.exp(-i * h)  #exact solution for y'=-y
         # y_exact.append(G * (1 / (1 + math.exp(-k * G * i * h) * (G / y0 - 1))))  # exact solution for y'=k*y*)G-y))
 
-    # plt.plot(t, y_heun, label="Heun method")
-    plt.plot(time_array, result_euler_explicit, label=["height (expl Euler)", "velocity (expl Euler"])
-    plt.plot(time_array, result_midpointrule, label=["height (Midpointrule)", "velocity (Midpointrule)"])
-    plt.plot(time_array, result_heun, label=["height (Heun)", "velocity (Heun)"])
-    # plt.plot(time_array, y_exact, label="Exact solution (exp)")
-
-    # plt.plot(t, y_exact, label="Exact solution y'=-y")
     plt.legend()
     plt.show()
