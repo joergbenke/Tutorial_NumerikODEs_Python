2a8ab9d4e7ce0baeba216f840fffe4d663e40477
Joerg Benke <j.benke@fz-juelich.de>
2023-10-02 06:23:18 +0200

    main.py and matrixcompilation file created
    No contents

---
+++ Chap6_8/heatequation_dirchlet_homogeneous/main.py
@@ -0,0 +1,7 @@
+from matrixcompilation import matrix_heat
+
+if __name__ == "__main__":
+    " Test of the solution of the heat equation"
+    
+    # definitions of parameters
+       
---
+++ Chap6_8/heatequation_dirchlet_homogeneous/matrixcompilation.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def matrix_heat():
+    "Compilation of the 2d discretized heat equation matrix"
